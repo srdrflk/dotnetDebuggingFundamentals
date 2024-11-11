6b59ca08bd84ebc799407a0ed798832ad082b8b9
srdrflk <55311271+srdrflk@users.noreply.github.com>
2024-11-11 14:32:31 +0300

    initial commit

---
+++ Task1/Product.cs
@@ -0,0 +1,24 @@
+﻿namespace Task1
+{
+    public class Product
+    {
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || this.GetType() != obj.GetType())
+                return false;
+
+            Product other = (Product)obj;
+            return (this.Name == other.Name) && (this.Price == other.Price);
+        }
+    }
+}
